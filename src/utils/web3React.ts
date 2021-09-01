7d56ba3214626aec4e0e68ea82b0ad97d75f6867
gabileibo <gabi.leibovich@gmail.com>
2021-09-01 20:41:26 +0200

    allow for testnet

--- src/utils/web3React.ts
+++ src/utils/web3React.ts
@@ -2,6 +2,7 @@ import { InjectedConnector } from '@web3-react/injected-connector'
 import { WalletConnectConnector } from '@web3-react/walletconnect-connector'
 import { BscConnector } from '@binance-chain/bsc-connector'
 import { ConnectorNames } from '@pancakeswap/uikit'
+import { ChainId } from '@pancakeswap/sdk'
 import { ethers } from 'ethers'
 import getNodeUrl from './getRpcUrl'
 
@@ -9,7 +10,7 @@ const POLLING_INTERVAL = 12000
 const rpcUrl = getNodeUrl()
 const chainId = parseInt(process.env.REACT_APP_CHAIN_ID, 10)
 
-const injected = new InjectedConnector({ supportedChainIds: [chainId] })
+const injected = new InjectedConnector({ supportedChainIds: [ChainId.MAINNET, ChainId.TESTNET] })
 
 const walletconnect = new WalletConnectConnector({
   rpc: { [chainId]: rpcUrl },
