092990be720759c13f9d4e71aa02dd2029aed4ae
Achthar <69119535+Achthar@users.noreply.github.com>
2021-09-16 01:50:08 +0200

    bignumber

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,6 +1,6 @@
 
 import BigNumber from 'bignumber.js/bignumber'
-import { BIG_TEN } from 'utils/bigNumber'
+// import { BIG_TEN } from 'utils/bigNumber'
 
 BigNumber.config({
   EXPONENTIAL_AT: 1000,
@@ -33,7 +33,7 @@ export const BASE_URL = 'https://pancakeswap.finance'
 export const BASE_ADD_LIQUIDITY_URL = `${BASE_URL}/add`
 export const BASE_LIQUIDITY_POOL_URL = `${BASE_URL}/pool`
 export const BASE_BSC_SCAN_URL = BASE_BSC_SCAN_URLS[ChainId.MAINNET_BSC]
-export const DEFAULT_TOKEN_DECIMAL = BIG_TEN.pow(18)
+export const DEFAULT_TOKEN_DECIMAL = (new BigNumber(10)).pow(18)
 export const DEFAULT_GAS_LIMIT = 200000
 export const AUCTION_BIDDERS_TO_FETCH = 500
 export const RECLAIM_AUCTIONS_TO_FETCH = 500
