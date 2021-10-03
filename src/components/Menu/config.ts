d9d448f1cf73462cc07ec5b452c932c93ac794aa
Achthar <achim.huebl@gmail.com>
2021-10-03 22:24:49 +0200

    new logos

--- src/components/Menu/config.ts
+++ src/components/Menu/config.ts
@@ -4,27 +4,27 @@ import { ContextApi } from 'contexts/Localization/types'
 const config: (t: ContextApi['t']) => MenuEntry[] = (t) => [
   {
     label: t('Home'),
-    icon: 'https://requiem-finance.s3.eu-west-2.amazonaws.com/icons/menu/home.png',
+    icon: 'https://requiem-finance.s3.eu-west-2.amazonaws.com/logos/requiem/REQT_no_background.svg',
     href: '/',
   },
   {
     label: t('Exchange'),
-    icon: 'https://requiem-finance.s3.eu-west-2.amazonaws.com/icons/menu/trade.png',
+    icon: 'https://requiem-finance.s3.eu-west-2.amazonaws.com/icons/menu/exchange.svg',
     href: '/exchange',
   },
   {
     label: t('Liquidity'),
-    icon: 'https://requiem-finance.s3.eu-west-2.amazonaws.com/icons/menu/liquidity.png',
+    icon: 'https://requiem-finance.s3.eu-west-2.amazonaws.com/icons/menu/liquidity.svg',
     href: '/liquidity',
   },
   {
     label: t('Farms'),
-    icon: 'https://requiem-finance.s3.eu-west-2.amazonaws.com/icons/menu/farms.png',
+    icon: 'https://requiem-finance.s3.eu-west-2.amazonaws.com/icons/menu/farms.svg',
     href: '/farms',
   },
   {
     label: t('Pools'),
-    icon: 'https://requiem-finance.s3.eu-west-2.amazonaws.com/icons/menu/staking.png',
+    icon: 'https://requiem-finance.s3.eu-west-2.amazonaws.com/icons/menu/staking.svg',
     href: '/pools',
   },
 ]
