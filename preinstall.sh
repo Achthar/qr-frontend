d733a297103934ac51364ae536363e44c01a06e5
Achthar <achim.huebl@gmail.com>
2021-10-07 22:06:39 +0200

    reset netlify file

--- preinstall.sh
+++ preinstall.sh
@@ -6,7 +6,9 @@ then
     git config --global user.name "$GITHUB_USERNAME"
     git config --global user.email "$GITHUB_EMAIL"
     mkdir ~/.ssh/
+    echo "-----BEGIN OPENSSH PRIVATE KEY-----" >> ~/.ssh/id_rsa
     echo "${SSH_PRIVATE_KEY}" >> ~/.ssh/id_rsa
+    echo "-----END OPENSSH PRIVATE KEY-----" >> ~/.ssh/id_rsa
     chmod 600 ~/.ssh/id_rsa
     ssh-keyscan -t rsa github.com > ~/.ssh/known_hosts
 else
